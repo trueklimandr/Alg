3c773d077c8046b6bce1bd25e76a99509905c200
Andrei Klimenko <klimandr@mail.ru>
2018-03-26 14:44:07 +0300

    Compare selection sort and insertion sort.

--- sort/basicSort.php
+++ sort/basicSort.php
@@ -44,4 +44,26 @@ class basicSort
             return 'Received wrong order';
         }
     }
+
+    /**
+     * @param array $a (input array for sort)
+     * @return array (sorted array)
+     */
+    public function selectionSort(array $a)
+    {
+        $aKeys = array_keys($a);
+        for ($j = 0; $j < count($a) - 1; $j++) {
+            $var = $a[$aKeys[$j]];
+            $key = $j;
+            for ($i = $j; $i < count($a) - 1; $i++) {
+                if ($a[$aKeys[$i]] > $a[$aKeys[$i + 1]] && $a[$aKeys[$i + 1]] < $var) {
+                    $var = $a[$aKeys[$i + 1]];
+                    $key = $i + 1;
+                }
+            }
+            $a[$aKeys[$key]] = $a[$aKeys[$j]];
+            $a[$aKeys[$j]] = $var;
+        }
+        return $a;
+    }
 }
